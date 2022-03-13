14260fbe86846558603c2522eb83d9bf7fe21387
L1nked2 <leeerjin@gmail.com>
2022-03-13 22:45:25 +0900

    220313 lock issue fixed

--- threads/thread.c
+++ threads/thread.c
@@ -180,13 +180,14 @@ thread_create (const char *name, int priority,
 
 	/* Initialize thread. */
 	init_thread (t, name, priority);
-	tid = t->tid = allocate_tid ();
-	
+
 	t->wakeup_tick = 0;
 
 	t->init_priority = priority;
 	t->wait_on_lock = NULL;
 	list_init(&t->holding_locks);
+
+	tid = t->tid = allocate_tid ();
 
 	/* Call the kernel_thread if it scheduled.
 	 * Note) rdi is 1st argument, and rsi is 2nd argument. */
