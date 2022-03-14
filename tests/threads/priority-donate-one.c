6a44f94176de04974a9a155bdfb9ab47c5b00f10
jngmnkm <38782911+wjdalsk0512@users.noreply.github.com>
2022-03-14 23:10:51 +0900

    220314 priority-donate-one fixed for debugging

--- tests/threads/priority-donate-one.c
+++ tests/threads/priority-donate-one.c
@@ -48,8 +48,14 @@ acquire1_thread_func (void *lock_)
   struct lock *lock = lock_;
 
   lock_acquire (lock);
+  /* for debugging */
+  printf("this should be printed if lock_acquire has no error\n");
+
   msg ("acquire1: got the lock");
   lock_release (lock);
+  /* for debugging */
+  printf("this should be printed if lock_releas has no error\n");
+  
   msg ("acquire1: done");
 }
 
