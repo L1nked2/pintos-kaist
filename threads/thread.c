79f3ebc8c3b126a26ab39873e67b91c6f37a743f
L1nked23 <leeerjin@gmail.com>
2022-03-14 21:24:33 +0900

    220314 boot issue

--- threads/thread.c
+++ threads/thread.c
@@ -103,7 +103,6 @@ thread_init (void) {
 	initial_thread = running_thread ();
 	init_thread (initial_thread, "main", PRI_DEFAULT);
 	initial_thread->status = THREAD_RUNNING;
-  //list_init(&initial_thread->holding_locks);
 	initial_thread->tid = allocate_tid ();
 }
 
@@ -405,7 +404,7 @@ init_thread (struct thread *t, const char *name, int priority) {
   t->wakeup_tick = 0;
 	t->init_priority = priority;
 	t->wait_on_lock = NULL;
-  //list_init(&t->holding_locks);
+  list_init(&t->holding_locks);
 	t->magic = THREAD_MAGIC;
 }
 
