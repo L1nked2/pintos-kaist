d0439c39a2abf4f12c657c48000efcd8b2a6d4fb
wjdalsk0512 <38782911+wjdalsk0512@users.noreply.github.com>
2022-04-13 01:34:06 +0900

    Update syscall.h

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -9,6 +9,9 @@ struct lock file_lock;
 void syscall_init (void);
 
 /* codes for project 2 */
+void validate_addr(const uint64_t *addr);
+bool validate_fd(int fd);
+static struct file *search_file(int fd);
 void sys_halt();
 void sys_exit(int status);
 tid_t sys_fork(const char *thread_name, struct intr_frame *if_);
