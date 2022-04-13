35e20d330c82994418f47ebc94b8cea9f5c9a5c8
L1nked23 <leeerjin@gmail.com>
2022-04-14 04:58:43 +0900

    Update thread.h

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -38,7 +38,7 @@ int load_avg, prev_load_avg;
 
 /* Default value for file descriptors */
 #define FD_NR_START_INDEX 3
-#define FD_MAX_INDEX 128
+#define FD_MAX_INDEX 32
 
 /* A kernel thread or user process.
  *
