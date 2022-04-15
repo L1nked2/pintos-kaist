16dc66cfe1cc0478c57edddd31c3034e20af3da1
wjdalsk0512 <38782911+wjdalsk0512@users.noreply.github.com>
2022-04-15 19:09:39 +0900

    Update process.c

--- userprog/process.c
+++ userprog/process.c
@@ -205,12 +205,11 @@ __do_fork (void *aux) {
     }
   }
 	
-	//process_init ();
+	process_init ();
 
   // copy is_user_thread flag
   current->is_user_thread = parent->is_user_thread;
   
-  sema_up(&current->load_sema);
 	/* Finally, switch to the newly created process. */
 	if (succ)
 		do_iret (&if_);
