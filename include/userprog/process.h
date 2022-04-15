20fad48fa7592f942a4de05bf4947a67c734587c
L1nked23 <leeerjin@gmail.com>
2022-04-16 02:41:47 +0900

    220416 dynamic fdt

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -2,6 +2,8 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
+#include <file.h>
+#include "threads/malloc.h"
 
 tid_t process_create_initd (const char *file_name);
 tid_t process_fork (const char *name, struct intr_frame *if_);
@@ -14,5 +16,11 @@ void process_activate (struct thread *next);
 int parse_args (char *raw_text, char**argv);
 void insert_args(int argc, char **argv, struct intr_frame *_if);
 struct thread* get_child_thread (tid_t tid);
+
+struct fd{
+  struct file* fp;            /* file pointer */
+  int index;                  /* index of file descriptor */
+  struct list_elem fd_elem;   /* file descriptor table list element */
+};
 
 #endif /* userprog/process.h */
