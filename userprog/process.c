a19708f08430583e240f985641ccb43e9dbb9ee6
L1nked23 <leeerjin@gmail.com>
2022-04-19 01:57:29 +0900

    Update process.c

--- userprog/process.c
+++ userprog/process.c
@@ -450,7 +450,7 @@ process_exit (void) {
     struct list_elem *e = list_pop_front (fdt);
     struct fd *fd_entry = list_entry(e, struct fd, fd_elem);
     if(fd_entry->fp != NULL && fd_entry->fp != STDIN && fd_entry->fp != STDOUT && fd_entry->dup_secure == true) {
-      file_close(fd_entry->fp);
+      free(fd_entry->fp);
     }
     free(fd_entry);
   }
