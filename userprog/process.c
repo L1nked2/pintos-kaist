561c950d5e0bebb265940d04f0b82398047f5265
L1nked23 <leeerjin@gmail.com>
2022-05-22 20:42:59 +0900

    Update process.c

--- userprog/process.c
+++ userprog/process.c
@@ -946,6 +946,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 		/* Advance. */
 		read_bytes -= page_read_bytes;
 		zero_bytes -= page_zero_bytes;
+    ofs += page_read_bytes;
 		upage += PGSIZE;
 	}
   //free(segment_info);
