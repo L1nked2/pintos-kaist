942827efa77241a8a177f545689e924b12804352
L1nked23 <leeerjin@gmail.com>
2022-05-22 22:33:38 +0900

    Update vm.c

--- vm/vm.c
+++ vm/vm.c
@@ -354,7 +354,6 @@ supplemental_page_table_kill (struct supplemental_page_table *spt UNUSED) {
 	/* TODO: Destroy all the supplemental_page_table hold by thread and
 	 * TODO: writeback all the modified contents to the storage. */
   // writeback will be implemented in each page type
-  return;///test
   if(hash_empty(&spt->pages)) {
     hash_destroy(&spt->pages, NULL);
   } else {
