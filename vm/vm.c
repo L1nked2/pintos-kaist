7a2312a22cd79c440c262f029d3122f13869d19e
L1nked23 <leeerjin@gmail.com>
2022-05-24 02:27:32 +0900

    Update vm.c

--- vm/vm.c
+++ vm/vm.c
@@ -325,11 +325,13 @@ supplemental_page_table_copy (struct supplemental_page_table *dst,
       // allocate page as uninit first
       if(!vm_alloc_page(type, va, writable))
         return false;
+      printf("ANON & FILE page copy CK1\n");///test
       // claim page immediately
-      struct page* dst_page = spt_find_page(dst, va);
       if(!vm_claim_page(va))
         return false;
+      printf("ANON & FILE page copy CK2\n");///test
       // copy contents of memory -> CoW will change this
+      struct page* dst_page = spt_find_page(dst, va);
       memcpy(dst_page->frame->kva, src_page->frame->kva, PGSIZE);
       printf("ANON & FILE page copy done\n");///test
     }
