3f227bcc9d6c84bba00fa965e81b8e2ae5fc6275
L1nked23 <leeerjin@gmail.com>
2022-05-24 23:25:00 +0900

    Update file.c

--- vm/file.c
+++ vm/file.c
@@ -1,7 +1,7 @@
 /* file.c: Implementation of memory backed file object (mmaped object). */
 
 #include "vm/vm.h"
-
+#include "userprog/syscall.h" // for file lock
 static bool file_backed_swap_in (struct page *page, void *kva);
 static bool file_backed_swap_out (struct page *page);
 static void file_backed_destroy (struct page *page);
