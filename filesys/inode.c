8816d71b41a133fe2388b3c0e26f6c46d51e0eea
L1nked23 <leeerjin@gmail.com>
2022-06-12 07:20:17 +0900

    Update inode.c

--- filesys/inode.c
+++ filesys/inode.c
@@ -176,11 +176,7 @@ inode_open (disk_sector_t sector) {
 	inode->open_cnt = 1;
 	inode->deny_write_cnt = 0;
 	inode->removed = false;
-#ifdef EFILESYS
-  disk_read (filesys_disk, cluster_to_sector(inode->sector), &inode->data);
-#else
   disk_read (filesys_disk, inode->sector, &inode->data);
-#endif
   return inode;
 }
 
