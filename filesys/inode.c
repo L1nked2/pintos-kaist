d9f38da6129c05c13d660a0693636e21087e2b97
L1nked23 <leeerjin@gmail.com>
2022-06-12 06:04:01 +0900

    Update inode.c

--- filesys/inode.c
+++ filesys/inode.c
@@ -243,9 +243,9 @@ inode_read_at (struct inode *inode, void *buffer_, off_t size, off_t offset) {
 
 	while (size > 0) {
 #ifdef EFILESYS
-		if (!inode->data.start) break;
-		cluster_t clst = fat_cluster_read_at(inode->data.start, offset);
-		disk_sector_t sector_idx = cluster_to_sector(clst);
+		//cluster_t clst = fat_cluster_read_at(inode->data.start, offset);
+		//disk_sector_t sector_idx = cluster_to_sector(clst);
+    disk_sector_t sector_idx = byte_to_sector (inode, offset);
 #else
 		/* Disk sector to read, starting byte offset within sector. */
 		disk_sector_t sector_idx = byte_to_sector (inode, offset);
